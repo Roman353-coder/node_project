cc7fdc1166e7029d4d9b679d2dc2027fcd66b2f6
Roman353-coder <romanks866@gmail.com>
2025-11-24 10:09:02 -0700

    Update

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const router = require('./routes/pages.js');
-const userRoute = require('./routes/users.js')
+const userRoute = require('./routes/users.js');
 const ejs = require('ejs');
+const rateLimit = require('express-rate-limit');
 const port = 3000;
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
-const limiter = rateLimitr({
+const limiter = rateLimit({
     windowMs: 1 * 15 * 1000,
     max: 11,
     message: "Too many requests, please try again later."
 })
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(limiter);
 app.use(router);
 app.use(userRoute);
 app.use(express.static('public'));
@@ -21,5 +24,3 @@ app.use(express.static('public'));
 app.listen(port, () =>{
     console.log(`Server is running on http://localhost:${port}`)
 });
-
-
