17be436b232a774e2de3f7cbd1347f645e8985f4
Roman353-coder <romanks866@gmail.com>
2025-11-24 09:49:13 -0700

    Update

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ const port = 3000;
 app.set('view engine','ejs');
 app.set('views', './views');
 
-app.get('/', (req, res )=> {
-    res.send("Hello,  World!");
-});
 
+const limiter = rateLimitr({
+    windowMs: 1 * 15 * 1000,
+    max: 11,
+    message: "Too many requests, please try again later."
+})
 app.use(router);
 app.use(userRoute);
+app.use(express.static('public'));
 
 app.listen(port, () =>{
     console.log(`Server is running on http://localhost:${port}`)
