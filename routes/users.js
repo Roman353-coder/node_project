1fc105b470fc9a34863df44d42af09adad28e664
romanks866 <romanks866@gmail.com>
2025-11-26 17:51:36 +0000

    Update

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,13 @@ router.get('/home', (req, res)=>{
 res.render('home');
 });
 
+router.get('/json-example', (req, res)=>{
+    res.json({message: "This is  a JSON response"})
+});
 
+router.get('/api/v1/users', (req, res) =>{
+
+});
 
 
 module.exports = router;
